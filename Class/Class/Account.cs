b66875d3385a03c8afdd80c57a83af2fec8d93ea
Ali200119 <aliit@code.edu.az>
2022-12-14 02:25:29 +0400

    feat: Task 1

---
+++ Class/Class/Account.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Class
+{
+	public class Account
+	{
+		public Account(string username, string password)
+		{
+			if (username == "cavid123" && password == "Cavid1993")
+			{
+				Console.WriteLine("Login completed successfully");
+			}
+
+			else
+			{
+				Console.WriteLine("Username or password is wrong");
+			}
+		}
+	}
+}
+
---
+++ Class/Class/Program.cs
@@ -0,0 +1,13 @@
+﻿#region Task 1
+
+//using Class;
+
+//Account user = new Account("cavid123", "Cavid1993");
+
+#endregion
+
+#region Task 2
+
+
+
+#endregion
